f1d362c1b95de8c4e1a67657e3da01cc4475046a
JinshiLintao <jslt0723@gmail.com>
2025-05-07 22:58:35 +0100

    initial commint

---
+++ .ipynb_checkpoints/app-checkpoint.py
@@ -0,0 +1,106 @@
+import streamlit as st
+import pandas as pd
+import shap
+import joblib
+import matplotlib.pyplot as plt
+# from shap_utils import plot_shap_values
+# st.set_page_config(page_title="Diagnosis App", layout="wide")  # Optional: wide layout
+
+st.markdown("""
+<style>
+.sidebar .sidebar-content {
+    background-color: #f0f2f6;
+}
+.stButton>button {
+    background-color: #4CAF50;
+    color:white;
+}
+</style>
+""", unsafe_allow_html=True)
+
+
+st.markdown("<h3 style='text-align: left;'>Predictive models for neurosyphilis assessment in six global diagnosis guildances</h3>", unsafe_allow_html=True)
+
+
+# Sidebar - Model selection
+guidance_options = {
+    "US 2018": "US2018_binary",
+    "US 2021": "US2021_binary",
+    "Up to date": "Uptodate",
+    "Europ": "Euro",
+    "China": "China_binary",
+    "Australia":"Aust_binary"
+}
+
+model_options = {
+    "Random Forest": "rf",
+    "SVC": "svc",
+    "NuSVC": "nusvc",
+    "Adaboost": "ada",
+    "XGBoost": "xgb"
+}
+
+
+selected_guidance = st.sidebar.selectbox("Select Diagnosis Guildances:", list(guidance_options.keys()))
+selected_model = st.sidebar.selectbox("Select Predictive Model:", list(model_options.keys()))
+
+
+# --- Create two tabs ---
+tab1, tab2 = st.tabs(["🧪 Prediction", "📊 ROC/AUC"])
+
+with tab1:
+    st.markdown("<h4>Enter Patient Information</h4>", unsafe_allow_html=True)
+
+    csf_pro = st.number_input("CSF Protein", min_value=0.0, max_value=1000.0, value=340.0)
+    csf_wbc = st.number_input("CSF WBC", min_value=0.0, max_value=1000.0, value=9.0)
+
+    binary_labels = [
+        'HIV_Ag', 'Serum_NTT', 'Serum_TPPA', 'Serum_IgG',
+        'CSF_IgG', 'CSF_TRUST/RPR', 'CSF_TPPA', 'CSF_VDRL', 'Neuro_Sympt'
+    ]
+
+    binary_values = {}
+    for label in binary_labels:
+        user_input = st.selectbox(f"{label}", ["Negative", "Positive"])
+        binary_values[label] = 1 if user_input == "Positive" else 0
+
+    input_data = {
+        'CSF_Pro': csf_pro,
+        'CSF_WBC': csf_wbc,
+        **binary_values
+    }
+
+    input_df = pd.DataFrame([input_data])
+
+    if st.button("Predict"):
+        guidance_folder = guidance_options[selected_guidance]
+        model_name = model_options[selected_model]
+        model_path = f"models/{guidance_folder}_{model_name}.pkl"
+
+        try:
+            model = joblib.load(model_path)
+            prob = model.predict_proba(input_df)[0][1]
+            st.write(f'Neurosyphilis for {selected_guidance} using {selected_model.lower()}')
+            st.metric(label="Predicted Probability", value=f"{prob:.2%}")
+
+        except Exception as e:
+            st.error(f"Error loading model or predicting: {e}")
+
+from PIL import Image
+import os
+
+with tab2:
+    st.markdown("<h4>ROC/AUC</h4>", unsafe_allow_html=True)
+    
+    guidance1 = guidance_options[selected_guidance]
+    model1 = model_options[selected_model].lower()
+    fig_filename = f"{guidance1} {model1} auc.tif"
+
+    fig_path = os.path.join("figures/", fig_filename)
+
+    if os.path.exists(fig_path):
+        st.image(Image.open(fig_path), caption=f"ROCAUC - {selected_guidance} ({selected_model})", use_column_width=True)
+    else:
+        st.warning(f"No images found for {selected_guidance} using {selected_model}.")
+
+
